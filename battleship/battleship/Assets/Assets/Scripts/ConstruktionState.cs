d2fca438a919ed8628f0bd0c3917a03cde17f497
pinrol <erik_oliver_engstrom@hotmail.com>
2015-09-27 00:01:36 +0200

    checkout
    before sleep

--- battleship/battleship/Assets/Assets/Scripts/ConstruktionState.cs
+++ battleship/battleship/Assets/Assets/Scripts/ConstruktionState.cs
@@ -38,20 +38,19 @@ namespace Assets.Scripts
 			//shipCount = gameObjectList.Length;*/
 			
 			
-			ComputerShipPlacement ();
-			Debug.Break ();
+			 ComputerShips ();
+
 			foreach(GameObject ship in gameObjectList){
-				Debug.Log(ship.name);
-				Debug.Break();
+
 				ship.gameObject.GetComponent<PlaceShips>().allowPlacement = true;
 			}
 
 		}
 
-		public void ComputerShipPlacement(){
+		public void  ComputerShips(){
 			validTilesPc = GameObject.Find ("Boards/Board2").GetComponent<Board> ().tilesP2;
 			GameObject[] PcSubmarine = GameObject.FindGameObjectsWithTag ("PcSubmarine");
-			GameObject[] PcDestroyer = GameObject.FindGameObjectsWithTag ("PcDestroyer");
+			/*GameObject[] PcDestroyer = GameObject.FindGameObjectsWithTag ("PcDestroyer");
 			GameObject[] PcCruiser = GameObject.FindGameObjectsWithTag ("PcCruiser");
 			GameObject[] PcBattleship = GameObject.FindGameObjectsWithTag ("PcBattleship");
 			//GameObject tempObject = GameObject.Find(mouseObject.transform.position.x + " , " + mouseObject.transform.position.y);
@@ -64,28 +63,71 @@ namespace Assets.Scripts
 			int listNumber;
 			Vector2 randomisedTile;
 			GameObject tempObject;
+			int checkLength ;
 			//PcSubmarine[0].transform.position = GameObject.Find( 15 + " , " + 0).transform.position);
 
 			/*foreach ( GameObject ship in PcBattleship){
 				startTile = GameObject.Find(ship.name+"/start");
 			}*/
-
+			List<Vector2> varabiableValidTiles = new List<Vector2> ();
+			int RandomIsHoriz;
+			Vector2 border = new Vector2 (0, 0);
 			foreach (GameObject ship in PcSubmarine) {
-				Debug.Log(ship.name);
+				checkLength = ship.GetComponent<PlaceShips>().shipSize - 1;
+				RandomIsHoriz = (int) Mathf.Round( Random.Range(0, 1));
+				if(RandomIsHoriz == 0){
+					ship.GetComponent<PlaceShips>().isHoriz = false;
+					border = new Vector2(0 , checkLength );
+				}else{
+					border = new Vector2(checkLength, 0);
+				}
+
+
+
 				listNumber = Random.Range (0, validTilesPc.Count);
+
+
+				foreach(Vector2 validTile in validTilesPc){
+
+					tempObject = GameObject.Find(validTile.x + " , " + validTile.y);
+
+
+
+					/*if(isHoriz) {
+						loopXstart = (int)Mathf.Max(15, randomisedTile.x - 1);
+						loopYstart = (int)Mathf.Max(0, randomisedTile.y - 1);
+						loopXend   = (int)Mathf.Min(boardSize - 1 + 15, endTile.x + 1);
+						loopYend   = (int)Mathf.Min(boardSize - 1, endTile.y + 1);
+					} else {
+						loopXstart = (int)Mathf.Max(15, endTile.x - 1);
+						loopYstart = (int)Mathf.Max(0, endTile.y - 1);
+						loopXend   = (int)Mathf.Min(boardSize - 1 + 15, startTile.x + 1);
+						loopYend   = (int)Mathf.Min(boardSize - 1, startTile.y + 1);
+					}*/
+
+					//for (int x = loopXstart; x <= loopXend; x++) {
+						//for (int y = loopYstart; y <= loopYend; y++) {*/
+					
+				}
+
 				randomisedTile = validTilesPc [listNumber];
 				tempObject = GameObject.Find (randomisedTile.x + " , " + randomisedTile.y);
 				//ship.transform.position = GameObject.Find( (int)Random.Range(15f, 25f) + " , " + 0).transform.position;
-				PcShipPlacement(ship, tempObject, randomisedTile, listNumber);
-				endTile = ship.gameObject.transform.position;
-				startTile = endTile;
+				ComputerShipPlacement(ship, tempObject, randomisedTile, listNumber);
+
 			}
 
 		}
+		
 
-		public void PcShipPlacement( GameObject ship, GameObject tempObject, Vector2 randomisedTile, int listNumber){
+		
+		public void ComputerShipPlacement( GameObject ship, GameObject tempObject, Vector2 randomisedTile, int listNumber){
 			ship.gameObject.transform.position = tempObject.transform.position;
 
+			if (ship.CompareTag("PcSubmarine")) {
+				endTile = ship.gameObject.transform.position;
+				startTile = endTile;
+			}
 
 
 			
@@ -99,27 +141,30 @@ namespace Assets.Scripts
 			
 			
 			if(isHoriz) {
-				loopXstart = (int)Mathf.Max(0, randomisedTile.x - 1);
+				loopXstart = (int)Mathf.Max(15, randomisedTile.x - 1);
 				loopYstart = (int)Mathf.Max(0, randomisedTile.y - 1);
-				loopXend   = (int)Mathf.Min(boardSize - 1, endTile.x + 1);
+				loopXend   = (int)Mathf.Min(boardSize - 1 + 15, endTile.x + 1);
 				loopYend   = (int)Mathf.Min(boardSize - 1, endTile.y + 1);
 			} else {
-				loopXstart = (int)Mathf.Max(0, endTile.x - 1);
+				loopXstart = (int)Mathf.Max(15, endTile.x - 1);
 				loopYstart = (int)Mathf.Max(0, endTile.y - 1);
-				loopXend   = (int)Mathf.Min(boardSize - 1, startTile.x + 1);
+				loopXend   = (int)Mathf.Min(boardSize - 1 + 15, startTile.x + 1);
 				loopYend   = (int)Mathf.Min(boardSize - 1, startTile.y + 1);
 			}
 			
 			
 			for (int x = loopXstart; x <= loopXend; x++) {
 				for (int y = loopYstart; y <= loopYend; y++) {
-					
-					
+					 
+					Debug.Log(listNumber + " this is listnumber");
+					Debug.Log(validTilesPc.Count + " before removeAt");
 					validTilesPc.RemoveAt(listNumber);
+					Debug.Log(validTilesPc.Count + " after removeat");
 					tempObject = GameObject.Find(x + " , " + y);
 					tempObject.GetComponent<Tile>().active = false;
 					tempObject.GetComponentInChildren<SpriteRenderer>().color = Color.red;
-					
+
+
 					//remove this tile from list
 				}
 
